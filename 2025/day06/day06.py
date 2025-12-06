460e9def617d55925cedf7975895d22678fbd842
Nick Hollinghurst <nick.hollinghurst@raspberrypi.com>
2025-12-06 12:37:11 +0000

    2025 day 6: style tweaks

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -12,30 +12,28 @@ total2 = 0
 for line in sys.stdin:
     # part 1 -- keep a running sum and product of each column of numbers; choose at the end
     ll=line.strip().split()
-    for i in range(len(ll)):
-        if ll[i][0] >= '0' and ll[i][0] <= '9':
-            n = int(ll[i])
-            if i >= len(sums):
-                sums.append(0)
-                products.append(1)
+    while len(ll) > len(sums):
+        sums.append(0)
+        products.append(1)
+    for (i,v) in enumerate(ll):
+        if v.isdigit():
+            n = int(v)
             sums[i] += n
             products[i] *= n
         else:
-            total += sums[i] if ll[i][0] == '+' else products[i]
+            total += sums[i] if v[0] == '+' else products[i]
 
     # part 2 -- keep a running decimal value for each column of characters (watch out for 0)
-    for i in range(len(line)):
-        while i >= len(colnum):
-            colnum.append(-1)
-        if line[i] >= '0' and line[i] <= '9':
-            colnum[i] = (10 * colnum[i] if colnum[i] > 0 else 0) + int(line[i])
-        elif line[i] == '+':
-            sum = 0
+    while len(line) > len(colnum):
+        colnum.append(-1)
+    for (i,c) in enumerate(line):
+        if c >= '0' and c <= '9':
+            colnum[i] = 10 * max(0, colnum[i]) + int(c)
+        elif c == '+':
             while i < len(colnum) and colnum[i] >= 0:
-                sum += colnum[i]
-                i += 1
-            total2 += sum
-        elif line[i] == '*':
+                total2 += colnum[i]
+                i += 1 # (this does not affect the for loop)
+        elif c == '*':
             prod = 1
             while i < len(colnum) and colnum[i] >= 0:
                 prod *= colnum[i]
